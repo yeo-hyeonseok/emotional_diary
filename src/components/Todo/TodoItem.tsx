fe321ff5f49f7f79a875164d5921469617a6d0ef
dugustlr@naver.com <dugustlr@naver.com>
2024-03-15 20:12:04 +0900

    [update][TodoItem] 수정 및 삭제 기능 추가

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -1,4 +1,6 @@
+import { useRecoilState } from "recoil";
 import * as Type from "store/interface";
+import { todoListState } from "store";
 import Recoil from "components/Common/Recoil";
 
 interface Props {
@@ -9,15 +11,31 @@ interface Props {
 export default function TodoItem(props: Props) {
   const { className, data } = props;
 
+  const [list, setList] = useRecoilState(todoListState);
+
+  const editItem = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let arr = list.map((item: Type.TodoItem) => ({ ...item }));
+    const index = list.findIndex((item: Type.TodoItem) => item.id === data.id);
+
+    arr[index].text = e.target.value;
+    setList(arr);
+  };
+
+  const deleteItem = () => {
+    const arr = list.filter((item: Type.TodoItem) => item.id !== data.id);
+    setList(arr);
+  };
+
   return (
     <div className={`${className} flex w-full gap-3`}>
       <input
         value={data.text}
+        onChange={editItem}
         autoFocus
         className="w-full rounded-lg bg-gray-200 bg-opacity-50 p-2 text-xl"
         type="text"
       />
-      <Recoil size="sm" />
+      <Recoil onClick={deleteItem} size="sm" />
     </div>
   );
 }
