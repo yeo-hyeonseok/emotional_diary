66d40e0ec434e3cd2ac69766a37cb2444a2471d0
dugustlr@naver.com <dugustlr@naver.com>
2024-03-16 21:41:56 +0900

    [update][TodoItem] 체크박스 및 isComplete 핸들러 추가

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -21,20 +21,42 @@ export default function TodoItem(props: Props) {
     setList(arr);
   };
 
+  const toggleIsComplete = () => {
+    let arr = list.map((item: Type.TodoItem) => ({ ...item }));
+    const index = list.findIndex((item: Type.TodoItem) => item.id === data.id);
+
+    arr[index].isComplete = !arr[index].isComplete;
+    setList(arr);
+  };
+
   const deleteItem = () => {
     const arr = list.filter((item: Type.TodoItem) => item.id !== data.id);
     setList(arr);
   };
 
   return (
-    <div className={`${className} flex w-full gap-3`}>
+    <div className={`${className} relative flex w-full gap-3`}>
       <input
         value={data.text}
         onChange={editItem}
         autoFocus
-        className="w-full rounded-lg bg-gray-200 bg-opacity-50 p-2 text-xl"
+        className="w-full rounded-lg bg-gray-200 bg-opacity-50 py-2 pl-[42px] pr-2 text-xl"
         type="text"
       />
+      <span className="absolute left-1.5 top-1/2 -translate-y-1/2">
+        <input
+          onChange={toggleIsComplete}
+          checked={data.isComplete}
+          type="checkBox"
+          id={data.id}
+          className="hidden"
+        />
+        <label
+          style={{ backgroundImage: "url('/images/recoil.png')" }}
+          htmlFor={data.id}
+          className={`${data.isComplete && "-rotate-90"} block h-[30px] w-[30px] cursor-pointer bg-contain bg-no-repeat duration-300`}
+        />
+      </span>
       <Recoil onClick={deleteItem} size="sm" />
     </div>
   );
