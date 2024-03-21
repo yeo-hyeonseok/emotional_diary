23c2083a7238ff7bfd6c02185810308a4d3650d5
dugustlr@naver.com <dugustlr@naver.com>
2024-03-21 20:03:34 +0900

    [update][TodoInput] 입력한 데이터가 공백일 경우 랜덤한 명언으로 대체
    [etc]
    - placeholder를 "Enter the text..."로 변경

--- src/components/Todo/TodoInput.tsx
+++ src/components/Todo/TodoInput.tsx
@@ -15,6 +15,23 @@ export default function TodoInput(props: Props) {
   const setList = useSetRecoilState(todoListState);
   const [input, setInput] = useState("");
 
+  function getRandomNum() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  const sayings = [
+    "When in doubt, choose change.",
+    "Nothing in more despicable than respect based on fear.",
+    "Great minds have purposes, others have wishes.",
+    "Daechung heugbaeg sajin-e geul sseumyeon myeong-eon gatda",
+    "A day without laughter is a day wasted.",
+    "love what you have.",
+    "Success usually comes to those who are too busy to be looking for it.",
+    "It is kind of fun to do the impossible.",
+    "The way to get started is to quit talking and begin doing.",
+    "I find that the harder I work, the more luck I seem to have.",
+  ];
+
   const addItem = () => {
     const id = uuidv4();
 
@@ -22,7 +39,8 @@ export default function TodoInput(props: Props) {
       ...prev,
       {
         id,
-        text: input,
+        text:
+          input.replace(/\s+/g, "") !== "" ? input : sayings[getRandomNum()],
         isComplete: false,
       },
     ]);
@@ -37,7 +55,7 @@ export default function TodoInput(props: Props) {
         value={input}
         className="w-full rounded-xl bg-gray-200 bg-opacity-50 px-4 placeholder:text-white"
         type="text"
-        placeholder="enter the text..."
+        placeholder="Enter the text..."
       />
       <Recoil onClick={addItem} size="md" />
     </div>
